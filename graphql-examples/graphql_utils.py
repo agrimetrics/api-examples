d60b95351af42a8228527a71c731ec26bb885c3d
Andrew Fernando <andrew.fernando@agrimetrics.co.uk>
2019-09-27 16:25:43 +0100

    Use new cursor behaviour in example.

--- graphql-examples/graphql_utils.py
+++ graphql-examples/graphql_utils.py
@@ -21,8 +21,16 @@ def get_data(query):
 
 
 def get_paged_data(query):
-    last_response = get_data(query)
-    while last_response["data"]["fields"]:
-        yield last_response
-        query["variables"]["cursor"] = last_response["data"]["fields"][-1]["cursor"]
+    while True:
         last_response = get_data(query)
+        yield last_response
+
+        if not last_response["data"]["fields"]:
+            break
+
+        last_cursor = last_response["data"]["fields"][-1]["cursor"]
+
+        if last_cursor is None:
+            break
+
+        query["variables"]["cursor"] = last_cursor
